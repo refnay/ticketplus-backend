e84c168e262370d91bc4f85306793ffa50154e0c
refnay <yanferlaura1@gmail.com>
2026-07-28 18:59:20 -0500

    #10 delete evento avance

--- src/Shared/Application/Command/BaseCommand.php
+++ src/Shared/Application/Command/BaseCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Shared\Application\Command;
 
+use App\Shared\Domain\Exceptions\CompanyRequired;
 use App\Shared\Domain\Session;
 
 class BaseCommand
@@ -16,5 +17,14 @@ class BaseCommand
     public function session(): Session
     {
         return $this->session;
+    }
+
+    public function ensureCompany(): void
+    {
+        if (!is_null($this->session->company())) {
+            return;
+        }
+
+        throw new CompanyRequired();
     }
 }
