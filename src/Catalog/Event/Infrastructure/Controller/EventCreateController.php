df554064e5b22e60ee6e648409a5121e33b8e556
refnay <yanferlaura1@gmail.com>
2026-08-01 19:27:16 -0500

    #12 company creator

---
+++ src/Catalog/Event/Infrastructure/Controller/EventCreateController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Catalog\Event\Infrastructure\Controller;
+
+use App\Catalog\Event\Application\Create\CreateEventCommand;
+use App\Shared\Application\MessageBus;
+use App\Shared\Domain\Session;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class EventCreateController extends AbstractController
+{
+    public function create(Request $request, Session $session, MessageBus $messageBus): JsonResponse
+    {
+        $session->allowed();
+
+        $command = CreateEventCommand::create($request->toArray());
+        $command->setSession($session);
+        
+        /** @var string $id */
+        $id = $messageBus->dispatch($command);
+
+        return new JsonResponse(['id' => $id]);
+    }
+}
