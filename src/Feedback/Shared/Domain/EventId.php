da800bb7591cce40c11779adb2d68216cc0746b1
refnay <yanferlaura1@gmail.com>
2026-08-09 18:23:54 -0500

    #14 regularizacion

---
+++ src/Feedback/Shared/Domain/EventId.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Feedback\Shared\Domain;
+
+use App\Shared\Domain\ValueObjects\UuidValueObject;
+use Override;
+
+class EventId extends UuidValueObject 
+{
+    #[Override]
+    public function validate(): void
+    {
+    }
+}
