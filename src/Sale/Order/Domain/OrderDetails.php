fedff82be4ab6f8d1eb2c49e0f6095d6e68ac89c
refnay <yanferlaura1@gmail.com>
2026-08-15 18:34:14 -0500

    #14 create order

--- src/Sale/Order/Domain/OrderDetails.php
+++ src/Sale/Order/Domain/OrderDetails.php
@@ -11,4 +11,29 @@ class OrderDetails extends ArrayValueObject
     public function validate(): void
     {
     }
+
+    public function fromPattern(string $event, string $day, string $zone): self
+    {
+        return new self([
+            'event' => $event,
+            'day' => $day,
+            'zone' => $zone,
+            'seats' => [],
+        ]);
+    }
+
+    public function event(): ?string
+    {
+        return isset($this->value['event']) ? (string) $this->value['event'] : null;
+    }
+
+    public function zone(): ?string
+    {
+        return isset($this->value['zone']) ? (string) $this->value['zone'] : null;
+    }
+
+    public function day(): ?string
+    {
+        return isset($this->value['day']) ? (string) $this->value['day'] : null;
+    }
 }
