a87d037ab22e0b40a891edc6738b45162d35e7f4
refnay <yanferlaura1@gmail.com>
2026-08-16 10:38:41 -0500

    #14 update

--- src/Sale/Payment/Application/Update/PaymentUpdater.php
+++ src/Sale/Payment/Application/Update/PaymentUpdater.php
@@ -4,21 +4,26 @@ namespace App\Sale\Payment\Application\Update;
 
 use App\Sale\Order\Domain\OrderId;
 use App\Sale\Order\Domain\Services\OrderFinder;
+use App\Sale\Payment\Domain\Events\PaymentUpdatedDomainEvent;
 use App\Sale\Payment\Domain\PaymentId;
 use App\Sale\Payment\Domain\PaymentRepository;
 use App\Sale\Payment\Domain\PaymentStatus;
-use App\Sale\Payment\Domain\Provider\PaymentProvider;
 use App\Sale\Payment\Domain\Services\PaymentFinder;
 use App\Sale\Shared\Domain\UserId;
+use App\Shared\Application\Messenger\EventBus;
+use App\Shared\Domain\Utils\Primitive\ArrayBuilder;
 
 class PaymentUpdater
 {
+    private ArrayBuilder $events;
+
     public function __construct(
         private PaymentFinder $paymentFinder,
         private OrderFinder $orderFinder,
         private PaymentRepository $repository,
-        private PaymentProvider $provider,
+        private EventBus $eventBus,
     ) {
+        $this->events = ArrayBuilder::generate();
     }
 
     public function __invoke(PaymentId $id, OrderId $orderId, UserId $userId, string $token): void
@@ -29,5 +34,8 @@ class PaymentUpdater
         $payment->changeStatus(PaymentStatus::processing());
 
         $this->repository->update($payment);
+
+        $this->events->add(new PaymentUpdatedDomainEvent($payment->id(), $orderId->value(), $userId->value(), $token));
+        $this->eventBus->dispatch(...$this->events->items());
     }
 }
