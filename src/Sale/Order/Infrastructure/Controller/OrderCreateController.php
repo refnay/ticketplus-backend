b34e78ff22a5a9193b611475e908aa4efd21251c
refnay <yanferlaura1@gmail.com>
2026-08-15 19:32:53 -0500

    #14 create order

---
+++ src/Sale/Order/Infrastructure/Controller/OrderCreateController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Sale\Order\Infrastructure\Controller;
+
+use App\Sale\Order\Application\Create\CreateOrderCommand;
+use App\Shared\Application\MessageBus;
+use App\Shared\Domain\Session;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class OrderCreateController extends AbstractController
+{
+    public function create(Request $request, Session $session, MessageBus $messageBus): JsonResponse
+    {
+        $session->allPermissions();
+
+        $command = CreateOrderCommand::create($request->toArray());
+        $command->setSession($session);
+        
+        /** @var string $id */
+        $id = $messageBus->dispatch($command);
+
+        return new JsonResponse(['id' => $id]);
+    }
+}
