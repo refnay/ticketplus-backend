60fe337a32c49e40a3211fbc93727d962ede7dd5
refnay <yanferlaura1@gmail.com>
2026-08-16 14:03:06 -0500

    #14 update

--- src/Sale/Order/Infrastructure/Console/ExpiredOrdersCommand.php
+++ src/Sale/Order/Infrastructure/Console/ExpiredOrdersCommand.php
@@ -5,24 +5,38 @@ namespace App\Sale\Order\Infrastructure\Console;
 use App\Sale\Order\Application\Expire\OrderExpirator;
 use App\Sale\Order\Domain\Order;
 use App\Sale\Order\Domain\OrderRepository;
+use App\Shared\Domain\TransactionService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
 
 final class ExpiredOrdersCommand extends Command
 {
-    public function __construct(private OrderExpirator $expirator, private OrderRepository $repository)
-    {
+    public function __construct(
+        private OrderExpirator $expirator,
+        private OrderRepository $repository,
+        private TransactionService $transaction,
+    ) {
         parent::__construct('app:orders:expired');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         /** @var Order[] $orders */
-        $orders = $this->repository->findAll();
-        
-        foreach ($orders as $order) {
-            $this->expirator->__invoke($order->id());
+        $orders = $this->repository->findExpireds();
+
+        try {
+            $this->transaction->begin();
+
+            foreach ($orders as $order) {
+                $this->expirator->__invoke($order->id());
+            }
+
+            $this->transaction->commit();
+        } catch (Throwable) {
+            $this->transaction->rollback();
+            return Command::FAILURE;
         }
 
         return Command::SUCCESS;
