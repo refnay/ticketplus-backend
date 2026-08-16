009e2953cb489a17134acf863f8002ecd7dcf9e7
refnay <yanferlaura1@gmail.com>
2026-08-16 10:35:39 -0500

    #14 update

--- src/Sale/Payment/Application/Update/PaymentUpdater.php
+++ src/Sale/Payment/Application/Update/PaymentUpdater.php
@@ -7,29 +7,25 @@ use App\Sale\Order\Domain\Services\OrderFinder;
 use App\Sale\Payment\Domain\PaymentId;
 use App\Sale\Payment\Domain\PaymentRepository;
 use App\Sale\Payment\Domain\PaymentStatus;
-use App\Sale\Payment\Domain\PaymentToken;
+use App\Sale\Payment\Domain\Provider\PaymentProvider;
 use App\Sale\Payment\Domain\Services\PaymentFinder;
 use App\Sale\Shared\Domain\UserId;
-use App\Shared\Domain\Utils\Primitive\ArrayBuilder;
 
 class PaymentUpdater
 {
-    private ArrayBuilder $events;
-
     public function __construct(
         private PaymentFinder $paymentFinder,
         private OrderFinder $orderFinder,
         private PaymentRepository $repository,
+        private PaymentProvider $provider,
     ) {
-        $this->events = ArrayBuilder::generate();
     }
 
-    public function __invoke(PaymentId $id, PaymentToken $token, OrderId $orderId, UserId $userId): void
+    public function __invoke(PaymentId $id, OrderId $orderId, UserId $userId, string $token): void
     {
         $order = $this->orderFinder->__invoke($orderId, $userId);
         $payment = $this->paymentFinder->__invoke($id, $order->id());
 
-        $payment->changeToken($token);
         $payment->changeStatus(PaymentStatus::processing());
 
         $this->repository->update($payment);
--- src/Sale/Payment/Application/Update/UpdatePaymentCommandHandler.php
+++ src/Sale/Payment/Application/Update/UpdatePaymentCommandHandler.php
@@ -4,22 +4,21 @@ namespace App\Sale\Payment\Application\Update;
 
 use App\Sale\Order\Domain\OrderId;
 use App\Sale\Payment\Domain\PaymentId;
-use App\Sale\Payment\Domain\PaymentToken;
 use App\Sale\Shared\Domain\UserId;
 
 class UpdatePaymentCommandHandler
 {
-    public function __construct(private PaymentUpdater $updater)
+    public function __construct(private PaymentUpdater $processor)
     {
     }
 
     public function __invoke(UpdatePaymentCommand $command): void
     {
-        $this->updater->__invoke(
+        $this->processor->__invoke(
             PaymentId::fromString($command->payment()),
-            PaymentToken::fromString($command->token()),
             OrderId::fromString($command->order()),
             UserId::fromString($command->session()->user()),
+            $command->token(),
         );
     }
 }
