68f137e646f37f2b6585c25fcaee2a6e028cefb4
refnay <yanferlaura1@gmail.com>
2026-08-16 10:14:59 -0500

    #14 update

--- src/Sale/Payment/Infrastructure/Provider/MercadoPagoProvider.php
+++ src/Sale/Payment/Infrastructure/Provider/MercadoPagoProvider.php
@@ -4,7 +4,6 @@ namespace App\Sale\Payment\Infrastructure\Provider;
 
 use App\Sale\Payment\Domain\Payment;
 use App\Sale\Payment\Domain\Provider\PaymentProvider;
-use App\Sale\Payment\Domain\Provider\ProviderList;
 use App\Sale\Payment\Domain\Provider\ProviderResponse;
 
 final class MercadoPagoPaymentProvider implements PaymentProvider
